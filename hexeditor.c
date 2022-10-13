c488ee727985c18cdd25bbce771658cd4be3953f
1-UP Nuke <65999898+1UPNuke@users.noreply.github.com>
2022-10-13 20:07:56 +0300

    Fixed the long help flag not being help

--- hexeditor.c
+++ hexeditor.c
@@ -49,7 +49,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--h") == 0) {
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
         printhelp(argv[0]);
         return 0;
     }
